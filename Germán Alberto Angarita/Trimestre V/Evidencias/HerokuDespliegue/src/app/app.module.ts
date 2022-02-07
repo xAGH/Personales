407bc680f75bf87008a7e31c434b67b66a8ecd07
Alejo <alejito23001@gmail.com>
2022-02-07 08:28:05 -0500

    Taller de despliegue con flask en heroku

---
+++ Germán Alberto Angarita/Trimestre V/Evidencias/HerokuDespliegue/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
