e6c37c7a8ec438899a7191ae7b58cb593ea6affd
Alejo <alejito23001@gmail.com>
2022-03-23 17:26:32 -0500

    Cristian

---
+++ Hugo Hernan Henao/Trimestre V/Talleres/EjercicioWord/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace EjercicioWord
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VentanaPrincipal));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Fuente = new System.Windows.Forms.TabPage();
+            this.Avanzado = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.Fuente.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Fuente);
+            this.tabControl1.Controls.Add(this.Avanzado);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // Fuente
+            // 
+            this.Fuente.Controls.Add(this.listBox1);
+            this.Fuente.Controls.Add(this.label3);
+            this.Fuente.Controls.Add(this.label2);
+            this.Fuente.Controls.Add(this.label1);
+            this.Fuente.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.Fuente, "Fuente");
+            this.Fuente.Name = "Fuente";
+            this.Fuente.UseVisualStyleBackColor = true;
+            // 
+            // Avanzado
+            // 
+            resources.ApplyResources(this.Avanzado, "Avanzado");
+            this.Avanzado.Name = "Avanzado";
+            this.Avanzado.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            resources.GetString("listBox1.Items"),
+            resources.GetString("listBox1.Items1"),
+            resources.GetString("listBox1.Items2"),
+            resources.GetString("listBox1.Items3"),
+            resources.GetString("listBox1.Items4"),
+            resources.GetString("listBox1.Items5"),
+            resources.GetString("listBox1.Items6"),
+            resources.GetString("listBox1.Items7"),
+            resources.GetString("listBox1.Items8"),
+            resources.GetString("listBox1.Items9"),
+            resources.GetString("listBox1.Items10"),
+            resources.GetString("listBox1.Items11"),
+            resources.GetString("listBox1.Items12"),
+            resources.GetString("listBox1.Items13"),
+            resources.GetString("listBox1.Items14"),
+            resources.GetString("listBox1.Items15"),
+            resources.GetString("listBox1.Items16"),
+            resources.GetString("listBox1.Items17"),
+            resources.GetString("listBox1.Items18"),
+            resources.GetString("listBox1.Items19"),
+            resources.GetString("listBox1.Items20"),
+            resources.GetString("listBox1.Items21"),
+            resources.GetString("listBox1.Items22"),
+            resources.GetString("listBox1.Items23"),
+            resources.GetString("listBox1.Items24"),
+            resources.GetString("listBox1.Items25"),
+            resources.GetString("listBox1.Items26"),
+            resources.GetString("listBox1.Items27"),
+            resources.GetString("listBox1.Items28"),
+            resources.GetString("listBox1.Items29"),
+            resources.GetString("listBox1.Items30"),
+            resources.GetString("listBox1.Items31"),
+            resources.GetString("listBox1.Items32"),
+            resources.GetString("listBox1.Items33"),
+            resources.GetString("listBox1.Items34"),
+            resources.GetString("listBox1.Items35"),
+            resources.GetString("listBox1.Items36"),
+            resources.GetString("listBox1.Items37"),
+            resources.GetString("listBox1.Items38"),
+            resources.GetString("listBox1.Items39"),
+            resources.GetString("listBox1.Items40"),
+            resources.GetString("listBox1.Items41"),
+            resources.GetString("listBox1.Items42"),
+            resources.GetString("listBox1.Items43"),
+            resources.GetString("listBox1.Items44"),
+            resources.GetString("listBox1.Items45"),
+            resources.GetString("listBox1.Items46"),
+            resources.GetString("listBox1.Items47"),
+            resources.GetString("listBox1.Items48"),
+            resources.GetString("listBox1.Items49"),
+            resources.GetString("listBox1.Items50"),
+            resources.GetString("listBox1.Items51"),
+            resources.GetString("listBox1.Items52"),
+            resources.GetString("listBox1.Items53"),
+            resources.GetString("listBox1.Items54"),
+            resources.GetString("listBox1.Items55"),
+            resources.GetString("listBox1.Items56"),
+            resources.GetString("listBox1.Items57"),
+            resources.GetString("listBox1.Items58"),
+            resources.GetString("listBox1.Items59"),
+            resources.GetString("listBox1.Items60"),
+            resources.GetString("listBox1.Items61"),
+            resources.GetString("listBox1.Items62"),
+            resources.GetString("listBox1.Items63"),
+            resources.GetString("listBox1.Items64"),
+            resources.GetString("listBox1.Items65"),
+            resources.GetString("listBox1.Items66"),
+            resources.GetString("listBox1.Items67"),
+            resources.GetString("listBox1.Items68"),
+            resources.GetString("listBox1.Items69"),
+            resources.GetString("listBox1.Items70"),
+            resources.GetString("listBox1.Items71"),
+            resources.GetString("listBox1.Items72"),
+            resources.GetString("listBox1.Items73"),
+            resources.GetString("listBox1.Items74"),
+            resources.GetString("listBox1.Items75"),
+            resources.GetString("listBox1.Items76"),
+            resources.GetString("listBox1.Items77"),
+            resources.GetString("listBox1.Items78"),
+            resources.GetString("listBox1.Items79"),
+            resources.GetString("listBox1.Items80"),
+            resources.GetString("listBox1.Items81"),
+            resources.GetString("listBox1.Items82"),
+            resources.GetString("listBox1.Items83"),
+            resources.GetString("listBox1.Items84"),
+            resources.GetString("listBox1.Items85"),
+            resources.GetString("listBox1.Items86"),
+            resources.GetString("listBox1.Items87"),
+            resources.GetString("listBox1.Items88"),
+            resources.GetString("listBox1.Items89"),
+            resources.GetString("listBox1.Items90"),
+            resources.GetString("listBox1.Items91"),
+            resources.GetString("listBox1.Items92"),
+            resources.GetString("listBox1.Items93"),
+            resources.GetString("listBox1.Items94"),
+            resources.GetString("listBox1.Items95"),
+            resources.GetString("listBox1.Items96"),
+            resources.GetString("listBox1.Items97"),
+            resources.GetString("listBox1.Items98"),
+            resources.GetString("listBox1.Items99"),
+            resources.GetString("listBox1.Items100"),
+            resources.GetString("listBox1.Items101"),
+            resources.GetString("listBox1.Items102"),
+            resources.GetString("listBox1.Items103"),
+            resources.GetString("listBox1.Items104"),
+            resources.GetString("listBox1.Items105"),
+            resources.GetString("listBox1.Items106"),
+            resources.GetString("listBox1.Items107"),
+            resources.GetString("listBox1.Items108"),
+            resources.GetString("listBox1.Items109"),
+            resources.GetString("listBox1.Items110"),
+            resources.GetString("listBox1.Items111"),
+            resources.GetString("listBox1.Items112"),
+            resources.GetString("listBox1.Items113"),
+            resources.GetString("listBox1.Items114"),
+            resources.GetString("listBox1.Items115"),
+            resources.GetString("listBox1.Items116"),
+            resources.GetString("listBox1.Items117"),
+            resources.GetString("listBox1.Items118"),
+            resources.GetString("listBox1.Items119"),
+            resources.GetString("listBox1.Items120"),
+            resources.GetString("listBox1.Items121"),
+            resources.GetString("listBox1.Items122"),
+            resources.GetString("listBox1.Items123"),
+            resources.GetString("listBox1.Items124"),
+            resources.GetString("listBox1.Items125"),
+            resources.GetString("listBox1.Items126"),
+            resources.GetString("listBox1.Items127"),
+            resources.GetString("listBox1.Items128"),
+            resources.GetString("listBox1.Items129"),
+            resources.GetString("listBox1.Items130"),
+            resources.GetString("listBox1.Items131"),
+            resources.GetString("listBox1.Items132"),
+            resources.GetString("listBox1.Items133"),
+            resources.GetString("listBox1.Items134"),
+            resources.GetString("listBox1.Items135"),
+            resources.GetString("listBox1.Items136"),
+            resources.GetString("listBox1.Items137"),
+            resources.GetString("listBox1.Items138"),
+            resources.GetString("listBox1.Items139"),
+            resources.GetString("listBox1.Items140"),
+            resources.GetString("listBox1.Items141")});
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // VentanaPrincipal
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VentanaPrincipal";
+            this.Load += new System.EventHandler(this.VentanaPrincipal_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Fuente.ResumeLayout(false);
+            this.Fuente.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Fuente;
+        private System.Windows.Forms.TabPage Avanzado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
